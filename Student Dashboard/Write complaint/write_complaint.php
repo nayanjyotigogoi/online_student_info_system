304b6568bf1f1b001e43e46f9f25350bffe65525
Nayan <90410611+loutist@users.noreply.github.com>
2023-06-07 00:41:36 +0530

    alert added

--- Student Dashboard/Write complaint/write_complaint.php
+++ Student Dashboard/Write complaint/write_complaint.php
@@ -1,12 +1,5 @@
 <?php
-session_start();
-
-// if (!isset($_SESSION['username'])) {
-//     header("location: write_complaint.php");
-// } elseif ($_SESSION['usertype'] == 'student') {
-//     header("location: write_complaint.php");
-// }
-
+error_reporting(0);
 $host = "localhost";
 $user = "root";
 $password = "";
@@ -14,111 +7,102 @@ $db = "miniproject";
 
 $data = mysqli_connect($host, $user, $password, $db);
 
-$message = ""; // Variable to store the display message
+if ($_POST['submit']) {
+    $filename = $_FILES["img_source"]["name"];
+    $tempname = $_FILES["img_source"]["tmp_name"];
+    $folder = "receipt_folder/" . $filename;
+    move_uploaded_file($tempname, $folder);
 
-if (isset($_POST['complaint'])) {
+    $id = $_POST['id'];
+    $month = $_POST['month'];
     $name = $_POST['name'];
-    $enrollment_id = $_POST['enrollment_id'];
-    $roomno = $_POST['roomNo'];
-    $number = $_POST['phone'];
-    $queryType = $_POST['queryType'];
-    $query = $_POST['message'];
+    $enrollmentid = $_POST['enrollment_id'];
+    $email = $_POST['email'];
+    $phoneno = $_POST['phone_no'];
+    $imgsource = $_POST['img_source'];
+    $transactionid = $_POST['transaction_id'];
 
-    $sql = "INSERT INTO user_complaint (name, enrollment_id, roomNo, phone, querytype, message) VALUES ('$name', '$enrollment_id', '$roomno', '$number', '$queryType', '$query')";
+    // Check if the data already exists in the table for the same month and enrollment ID
+    $checkDuplicateQuery = "SELECT * FROM receipt WHERE month = '$month' AND enrollment_id = '$enrollmentid'";
+    $checkDuplicateResult = mysqli_query($data, $checkDuplicateQuery);
+    $duplicateRows = mysqli_num_rows($checkDuplicateResult);
 
-    $result = mysqli_query($data, $sql);
-
-    if ($result) {
-        $message = "Complaint registered successfully.";
+    if ($duplicateRows > 0) {
+        echo "<script> alert('Duplicate entry, data already exists for the same month and enrollment ID'); window.location.href = 'studenthome.php';</script>";
     } else {
-        $message = "Failed to register complaint. Please try again.";
+        $sql = "INSERT INTO receipt (id, month, name, enrollment_id, email, phone_no, img_source, transaction_id) VALUES ('" . $id . "','" . $month . "','" . $name . "','" . $enrollmentid . "','" . $email . "','" . $phoneno . "','" . $folder . "','" . $transactionid . "')";
+
+        $result = mysqli_query($data, $sql);
+
+        if ($result) {
+            $message = "Receipt uploaded successfully.";
+        } else {
+            $message = "Unable to Upload receipt. Please try again.";
+        }
     }
 }
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <title>Write Complaint</title>
-
-    <link rel="stylesheet" type="text/css" href="admin.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>File upload</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <?php
-    include "write_complaint_sidebar.php";
-    ?>
-
+    <?php include "file_upload_sidebar.php"; ?>
     <div class="content">
-        <p>You are encouraged to write down any problems or difficulties you are experiencing within your room. Whether it's related to noise disturbances, maintenance issues, conflicts with roommates, or any other concern, please feel free to write your Query. We value your feedback and want to ensure that your living conditions contribute positively to your overall college experience.</p>
+        <?php if (!empty($message)) : ?>
+            <div class="alert alert-<?php echo $result ? 'success' : 'danger'; ?>" role="alert">
+                <?php echo $message; ?>
+            </div>
+        <?php endif; ?>
 
-        <div>
-            <?php if (!empty($message)) : ?>
-                <div class="alert alert-success" role="alert">
-                    <?php echo $message; ?>
-                </div>
-            <?php endif; ?>
-            <form action="#" method="post">
-                <div class="form-group row">
-                    <label for="inputname" class="col-sm-1 col-form-label">Name</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputname" placeholder="Your name" name="name" required>
-                    </div>
-                </div>
-                <br>
-
-                <div class="form-group row">
-                    <label for="inputname" class="col-sm-1 col-form-label">Enrollment Id</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputname" placeholder="Your Enrollment Id" name="enrollment_id" required>
-                    </div>
-                </div>
-                <br>
-
-                <div class="form-group row">
-                    <label for="inputRoom-no" class="col-sm-1 col-form-label">Room-no</label>
-                    <div class="col-sm-10">
-                        <input type="number" class="form-control" id="inputroom-no" placeholder="Your room-no" name="roomNo" required>
-                    </div>
-                </div>
-                <br>
-                <div class="form-group row">
-                    <label for="inputPhone" class="col-sm-1 col-form-label">Phone number</label>
-                    <div class="col-sm-10">
-                        <input type="number" class="form-control" id="inputnumber" placeholder="phone" name="phone" required>
-                    </div>
-                </div>
-                <br>
-
-                <div class="form-group row">
-                    <label for="queryType" class="col-sm-1 col-form-label">Query Type</label>
-                    <div class="col-sm-10">
-                        <select class="form-select" id="queryType" name="queryType" required>
-                            <option value="" selected disabled>Select Query Type</option>
-                            <option value="Noise Disturbances">Noise Disturbances</option>
-                            <option value="Maintenance Issues">Maintenance Issues</option>
-                            <option value="Conflicts with Roommates">Conflicts with Roommates</option>
-                            <option value="Other">Other</option>
-                        </select>
-                    </div>
-                </div>
-                <br>
-
-                <div class="form-group">
-                    <label for="exampleFormControlTextarea1" class="col-sm-1 col-form-label">Your Query</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Write Down Your Query Here, explain in brief." name="message" required></textarea>
-                </div>
-
-                <br>
-                <button type="submit" class="btn btn-secondary" name="complaint">Write Complaint</button>
-            </form>
-        </div>
+        <h1>Upload your fee receipt here. Also, don't forget to mention the transaction Id.</h1>
+        <form action="file_upload_check.php" method="post" enctype="multipart/form-data">
+            <div>
+                <label>Month</label>
+                <input type="text" class="form-control" name="month" placeholder="Current month for paying receipt" required>
+            </div>
+            <br>
+            <div>
+                <label>Name</label>
+                <input type="name" class="form-control" name="name" placeholder="Your name here" required>
+            </div>
+            <br>
+            <div>
+                <label>Enrollment Id</label>
+                <input type="text" class="form-control" name="enrollment_id" placeholder="Your Enrollment Id" required>
+            </div>
+            <br>
+            <div>
+                <label>Email</label>
+                <input type="email" class="form-control" name="email" placeholder="Your Email" required>
+            </div>
+            <br>
+            <div>
+                <label>Phone-no</label>
+                <input type="number" class="form-control" name="phone_no" placeholder="Your contact no" required>
+            </div>
+            <br>
+            <label class="form-label" for="customFile">Upload your fee receipt here</label>
+            <br>
+            <input type="file" class="form-control" id="customFile" name="img_source" required>
+            <br>
+            <div>
+                <label>UPI Transaction Id</label>
+                <input type="text" class="form-control" name="transaction_id" placeholder="Your UPI Transaction Id" required>
+            </div>
+            <br>
+            <input type="submit" name="submit" value="Upload file">
+        </form>
     </div>
-
 </body>
+
 </html>
